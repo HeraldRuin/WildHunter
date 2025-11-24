e4724705b0364017094cb224363438db5fa1bd0c
melvin-rulit <marchenko1985g@outlook.com>
2025-11-24 15:55:55 +0000

    шаблон для списка животных

---
+++ bc-cms/themes/BC/Hotel/Views/frontend/layouts/search/fields/animals.blade.php
@@ -0,0 +1,44 @@
+@php($location_search_style = setting_item('hotel_location_search_style'))
+
+<div class="form-group">
+    <i class="field-icon icofont-paw"></i>
+    <div class="form-content">
+        <label> {{ $field['title'] }} </label>
+        @if($location_search_style=='autocompletePlace')
+            <div class="g-map-place" >
+                <input type="text" name="map_place" placeholder="{{__("Who will be hunted")}}"  value="{{request()->input('map_place')}}" class="form-control border-0">
+                <div class="map d-none" id="map-{{\Illuminate\Support\Str::random(10)}}"></div>
+                <input type="hidden" name="map_lat" value="{{request()->input('map_lat')}}">
+                <input type="hidden" name="map_lng" value="{{request()->input('map_lng')}}">
+            </div>
+
+        @else
+                <?php
+                $list_animals = $modelBlock['list_animals'] ?? [];
+                $animal_name = "";
+                $list_json = [];
+                $traverse = function ($animals, $prefix = '') use (&$traverse, &$list_json, &$animal_name) {
+                    foreach ($animals as $animal) {
+                        //$translate = $animal->translate();
+                        if (Request::query('animal_id') == $animal->id) {
+//                            $animal_name = $translate->name;
+                        }
+                        $list_json[] = [
+                            'id'    => $animal->id,
+                            'title' => $animal->name,
+                        ];
+                        $traverse($animal->children, $prefix . '-');
+                    }
+                };
+
+                $traverse($list_animals);
+                ?>
+
+        <div class="smart-search">
+            <input type="text" class="smart-search-location parent_text form-control" {{ ( empty(setting_item("hotel_location_search_style")) or setting_item("hotel_location_search_style") == "normal" ) ? "readonly" : ""  }} placeholder="{{__("Who will be hunted")}}" value="{{ $animal_name }}" data-onLoad="{{__("Loading...")}}"
+                   data-default="{{ json_encode($list_json) }}">
+            <input type="hidden" class="child_id" name="animal_id" value="{{Request::query('animal_id')}}">
+        </div>
+            @endif
+    </div>
+</div>
