2c1ce484ac6524e034ee3773e2a55a346ede9e0e
melvin-rulit <marchenko1985g@outlook.com>
2025-11-27 10:22:34 +0000

    модель и миграция(с новыми полями)

--- bc-cms/modules/Animals/Models/Animal.php
+++ bc-cms/modules/Animals/Models/Animal.php
@@ -10,5 +10,14 @@ class Animal extends Bookable
     use NodeTrait;
     protected $table = 'bc_animals';
 
+    public static function isEnable(): bool
+    {
+        return setting_item('animal_disable') == false;
+    }
+
+    public function getNumberReviewsInService($status = false)
+    {
+        return $this->reviewClass::countReviewByServiceID($this->id, false, $status, $this->type) ?? 0;
+    }
 }
 
