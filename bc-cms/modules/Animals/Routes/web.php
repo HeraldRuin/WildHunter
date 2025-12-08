d1545eb589141a448b22c17c9e4a7c3425b941a7
melvin-rulit <marchenko1985g@outlook.com>
2025-12-08 11:02:34 +0000

    поправка маршрутов для животных

--- bc-cms/modules/Animals/Routes/web.php
+++ bc-cms/modules/Animals/Routes/web.php
@@ -7,12 +7,12 @@ Route::group(['prefix'=>config('animal.animal_route_prefix')],function(){
 });
 
 Route::group(['prefix'=>'user/'.config('animal.animal_route_prefix'),'middleware' => ['auth','verified']],function(){
-    Route::get('/','ManageAnimalController@manageCar')->name('animal.vendor.index');
-    Route::get('/create','ManageAnimalController@createCar')->name('animal.vendor.create');
-    Route::get('/edit/{id}','ManageAnimalController@editCar')->name('animal.vendor.edit');
-    Route::get('/del/{id}','ManageAnimalController@deleteCar')->name('animal.vendor.delete');
+    Route::get('/','ManageAnimalController@manageAnimal')->name('animal.vendor.index');
+    Route::get('/create','ManageAnimalController@create')->name('animal.vendor.create');
+    Route::get('/edit/{id}','ManageAnimalController@edit')->name('animal.vendor.edit');
+    Route::get('/del/{id}','ManageAnimalController@delete')->name('animal.vendor.delete');
     Route::post('/store/{id}','ManageAnimalController@store')->name('animal.vendor.store');
-    Route::get('bulkEdit/{id}','ManageAnimalController@bulkEditCar')->name("animal.vendor.bulk_edit");
+    Route::get('bulkEdit/{id}','ManageAnimalController@bulkEdit')->name("animal.vendor.bulk_edit");
     Route::get('/booking-report/bulkEdit/{id}','ManageAnimalController@bookingReportBulkEdit')->name("animal.vendor.booking_report.bulk_edit");
     Route::get('/recovery','ManageAnimalController@recovery')->name('animal.vendor.recovery');
     Route::get('/restore/{id}','ManageAnimalController@restore')->name('animal.vendor.restore');
