e4d41e2cfa7839e8060007cac9a603736564761c
melvin-rulit <marchenko1985g@outlook.com>
2025-12-09 17:29:23 +0000

    контролер для отеля

--- bc-cms/themes/BC/Hotel/Views/frontend/layouts/details/hotel-rooms.blade.php
+++ bc-cms/themes/BC/Hotel/Views/frontend/layouts/details/hotel-rooms.blade.php
@@ -265,12 +265,6 @@
 <div class="modal fade" id="confirmSingleHotelBooking" tabindex="-1" role="dialog" aria-labelledby="confirmSingleBookingLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="confirmSingleBookingLabel">Подтверждение бронирования</h5>
-                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Закрыть">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
             <div class="modal-body text-center">
                 <p id="confirmSingleBookingText"></p>
             </div>
