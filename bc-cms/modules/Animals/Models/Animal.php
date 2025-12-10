9cc703676d25731194aa0136b97b5c7edf6168e7
melvin-rulit <marchenko1985g@outlook.com>
2025-12-10 11:32:59 +0000

    добавляем id отеля в бронирование для животных

--- bc-cms/modules/Animals/Models/Animal.php
+++ bc-cms/modules/Animals/Models/Animal.php
@@ -43,50 +43,20 @@ class Animal extends Bookable
 
         // Add Booking
         $adults = $request->input('adults');
-
         $start_date = new \DateTime($request->input('start_date'));
         $hotelId = $request->input('hotel_id');
         $animal_id = $request->input('animal_id');
 
-         AnimalBooking::create([
-            'animal_id' => $animal_id,
-            'user_id'   =>  auth()->id(),
-            'hotel_id'  => $hotelId,
-            'date'      => $start_date,
-            'adults'    => $adults,
-            'status'    => 'processing'
-        ]);
-
-//        $booking = new Booking();
-//        $booking->object_model = 'hunting'; // или имя модели как в get_bookable_services()
-//        $booking->object_id = $service->id;
-//        $booking->customer_id = $customer->id;
-//        $booking->vendor_id = $service->create_user ?? $service->vendor_id; // владелец сервиса
-//        $booking->start_date = $startDate; // "2025-12-15"
-//        $booking->end_date   = $endDate;   // "2025-12-17"
-//        $booking->total = $totalPrice; // сумма брони
-//        $booking->first_name = $customer->first_name;
-//        $booking->last_name  = $customer->last_name;
-//        $booking->email      = $customer->email;
-//        $booking->status     = Booking::CONFIRMED; // или DRAFT/UNPAID
-//        $booking->total_guests = $guestsCount ?? 1;
-
         $booking = new $this->bookingClass();
-        $booking->status = 'draft';
+        $booking->status = 'processing';
         $booking->object_id = $request->input('service_id');
         $booking->object_model = $request->input('service_type');
         $booking->vendor_id = $this->author_id;
         $booking->customer_id = Auth::id();
 //        $booking->total = $total;
-//        $booking->total_guests = $total_guests;
+        $booking->total_guests = $adults;
         $booking->start_date = $start_date->format('Y-m-d H:i:s');
-//        $booking->end_date = $end_date->format('Y-m-d H:i:s');
-
-        $booking->vendor_service_fee_amount = $total_service_fee ?? '';
-        $booking->vendor_service_fee = $list_service_fee ?? '';
-        $booking->buyer_fees = $list_buyer_fees ?? '';
-//        $booking->total_before_fees = $total_before_fees;
-//        $booking->total_before_discount = $total_before_fees;
+        $booking->hotel_id = $hotelId;
 
         $check = $booking->save();
 
