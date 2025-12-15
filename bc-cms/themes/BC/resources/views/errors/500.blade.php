196092d82f7b448aef9ecbea24f57c5f24822fef
melvin-rulit <marchenko1985g@outlook.com>
2025-12-15 09:57:23 +0000

    убрал ненужное при 500 ошибке

--- bc-cms/themes/BC/resources/views/errors/500.blade.php
+++ bc-cms/themes/BC/resources/views/errors/500.blade.php
@@ -1,4 +1,4 @@
 @extends('errors.illustrated-layout',['title'=>__('Page not found')])
 @section('title', __('Server Error'))
 @section('code', '500')
-@section('message', __('Server Error'))g'))
+@section('message', __('Server Error'))
