d37b077097bae10cbe61001cefda15e73db916b9
melvin-rulit <marchenko1985g@outlook.com>
2025-12-17 14:59:41 +0000

    правка шаблонов информации о бронировании

--- bc-cms/themes/BC/Booking/Views/frontend/booking/checkout-form.blade.php
+++ bc-cms/themes/BC/Booking/Views/frontend/booking/checkout-form.blade.php
@@ -56,47 +56,6 @@
                     <input type="text" placeholder="{{__("Your Phone")}}" class="form-control" value="{{$user->phone ?? ''}}" name="phone">
                 </div>
             </div>
-            <div class="col-md-6 field-address-line-1">
-                <div class="form-group">
-                    <label >{{__("Address line 1")}} </label>
-                    <input type="text" placeholder="{{__("Address line 1")}}" class="form-control" value="{{$user->address ?? ''}}" name="address_line_1">
-                </div>
-            </div>
-            <div class="col-md-6 field-address-line-2">
-                <div class="form-group">
-                    <label >{{__("Address line 2")}} </label>
-                    <input type="text" placeholder="{{__("Address line 2")}}" class="form-control" value="{{$user->address2 ?? ''}}" name="address_line_2">
-                </div>
-            </div>
-            <div class="col-md-6 field-city">
-                <div class="form-group">
-                    <label >{{__("City")}} </label>
-                    <input type="text" class="form-control" value="{{$user->city ?? ''}}" name="city" placeholder="{{__("Your City")}}">
-                </div>
-            </div>
-            <div class="col-md-6 field-state">
-                <div class="form-group">
-                    <label >{{__("State/Province/Region")}} </label>
-                    <input type="text" class="form-control" value="{{$user->state ?? ''}}" name="state" placeholder="{{__("State/Province/Region")}}">
-                </div>
-            </div>
-            <div class="col-md-6 field-zip-code">
-                <div class="form-group">
-                    <label >{{__("ZIP code/Postal code")}} </label>
-                    <input type="text" class="form-control" value="{{$user->zip_code ?? ''}}" name="zip_code" placeholder="{{__("ZIP code/Postal code")}}">
-                </div>
-            </div>
-            <div class="col-md-6 field-country">
-                <div class="form-group">
-                    <label >{{__("Country")}} <span class="required">*</span> </label>
-                    <select name="country" class="form-control">
-                        <option value="">{{__('-- Select --')}}</option>
-                        @foreach(get_country_lists() as $id=>$name)
-                            <option @if(($user->country ?? '') == $id) selected @endif value="{{$id}}">{{$name}}</option>
-                        @endforeach
-                    </select>
-                </div>
-            </div>
             <div class="col-md-12">
                 <label >{{__("Special Requirements")}} </label>
                 <textarea name="customer_notes" cols="30" rows="6" class="form-control" placeholder="{{__('Special Requirements')}}"></textarea>
