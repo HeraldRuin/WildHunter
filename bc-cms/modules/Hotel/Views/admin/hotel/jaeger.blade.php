c4abe8712a76a60570cb79f08bdb71aa840c8713
melvin-rulit <marchenko1985g@outlook.com>
2025-12-17 07:13:45 +0000

    ввод только цифр

--- bc-cms/modules/Hotel/Views/admin/hotel/jaeger.blade.php
+++ bc-cms/modules/Hotel/Views/admin/hotel/jaeger.blade.php
@@ -3,7 +3,10 @@
     <div class="panel-body">
         <div class="form-group">
             <label>{{__("Indicate the number of rangers for this base")}}</label>
-            <input type="text" value="{{$row->max_hunts_per_day}}"  name="max_hunts_per_day" class="form-control">
+            <input type="number" value="{{$row->max_hunts_per_day}}"  name="max_hunts_per_day" class="form-control" min="0"
+                   step="1"
+                   inputmode="numeric"
+                   oninput="this.value = this.value.replace(/[^0-9]/g, '')">
         </div>
     </div>
 </div>
