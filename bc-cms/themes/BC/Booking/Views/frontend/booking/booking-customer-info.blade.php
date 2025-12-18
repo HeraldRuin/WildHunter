b1a44df0e7b58fecdf236d3efa4fb277e2821717
melvin-rulit <marchenko1985g@outlook.com>
2025-12-18 18:37:05 +0000

    убрал ненужное из шаблона после бронирования

--- bc-cms/themes/BC/Booking/Views/frontend/booking/booking-customer-info.blade.php
+++ bc-cms/themes/BC/Booking/Views/frontend/booking/booking-customer-info.blade.php
@@ -21,32 +21,8 @@
                         <div class="val">{{$booking->phone}}</div>
                     </li>
                     <li class="info-address">
-                        <div class="label">{{__('Address line 1')}}</div>
+                        <div class="label">{{__('Address')}}</div>
                         <div class="val">{{$booking->address}}</div>
-                    </li>
-                    <li class="info-address2">
-                        <div class="label">{{__('Address line 2')}}</div>
-                        <div class="val">{{$booking->address2}}</div>
-                    </li>
-                    <li class="info-city">
-                        <div class="label">{{__('City')}}</div>
-                        <div class="val">{{$booking->city}}</div>
-                    </li>
-                    <li class="info-state">
-                        <div class="label">{{__('State/Province/Region')}}</div>
-                        <div class="val">{{$booking->state}}</div>
-                    </li>
-                    <li class="info-zip-code">
-                        <div class="label">{{__('ZIP code/Postal code')}}</div>
-                        <div class="val">{{$booking->zip_code}}</div>
-                    </li>
-                    <li class="info-country">
-                        <div class="label">{{__('Country')}}</div>
-                        <div class="val">{{get_country_name($booking->country)}}</div>
-                    </li>
-                    <li class="info-notes">
-                        <div class="label">{{__('Special Requirements')}}</div>
-                        <div class="val">{{$booking->customer_notes}}</div>
                     </li>
                 </ul>
             </div>
