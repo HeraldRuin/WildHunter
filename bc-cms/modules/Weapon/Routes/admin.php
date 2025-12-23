1309c30dd851fe0f5006dafacbb65778f9626428
melvin-rulit <marchenko1985g@outlook.com>
2025-12-23 08:35:16 +0000

    массовое действие для оружия

--- bc-cms/modules/Weapon/Routes/admin.php
+++ bc-cms/modules/Weapon/Routes/admin.php
@@ -5,6 +5,7 @@ Route::get('/','WeaponController@index')->name('weapon.admin.index');
 Route::get('/create','WeaponController@create')->name('weapon.admin.create');
 Route::get('/edit/{id}','WeaponController@edit')->name('weapon.admin.edit');
 Route::post('/store/{id}','WeaponController@store')->name('weapon.admin.store');
+Route::post('/bulkEdit','WeaponController@bulkEdit')->name('weapon.admin.bulkEdit');
 Route::get('/recovery','WeaponController@recovery')->name('weapon.admin.recovery');
 
 Route::get('/caliber','CaliberController@index')->name('caliber.admin.index');
