b2f2b434e981d215872e647431d5da9bc2d0ef36
melvin-rulit <marchenko1985g@outlook.com>
2025-12-23 07:28:02 +0000

    модель оружия

--- bc-cms/modules/Weapon/Models/WeaponType.php
+++ bc-cms/modules/Weapon/Models/WeaponType.php
@@ -4,13 +4,21 @@ namespace Modules\Weapon\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Modules\Booking\Models\Bookable;
+use Modules\Booking\Models\Booking;
 use Modules\User\Models\User;
 
-class WeaponType extends Model
+class WeaponType extends Bookable
 {
     protected $table = 'bc_weapons';
 
     protected $fillable = ['name', 'type', 'description'];
+    protected $translation_class = WeaponTranslation::class;
+
+    public static function isEnable(): bool
+    {
+        return true;
+    }
 
     public function users(): HasMany
     {
