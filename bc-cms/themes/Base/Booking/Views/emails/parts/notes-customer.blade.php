d4516724b7a841d0b6656b642066ab7265682e63
melvin-rulit <marchenko1985g@outlook.com>
2026-01-27 11:40:01 +0000

    шаблон особых параметров

--- bc-cms/themes/Base/Booking/Views/emails/parts/notes-customer.blade.php
+++ bc-cms/themes/Base/Booking/Views/emails/parts/notes-customer.blade.php
@@ -1,10 +1,4 @@
- @php
-    $customerNotes = $booking->customer_notes;
-@endphp
-
-<div class="b-panel">
-    <div class="b-panel-title">{{__('Customer notes')}}</div>
-    <div class="mt30">
-        <p>{{$customerNotes}}</p>
-    </div>
-</div>
+ <div class="b-panel-title">{{__('Customer notes')}}</div>
+ <div class="mt30">
+     <p>{{$booking->customer_notes}}</p>
+ </div>
