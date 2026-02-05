5b84b02a579cbbc1b1c2ffae533f9930e8ce5aad
melvin-rulit <melvin.melvi.rulit@gmail.com>
2026-02-05 13:43:56 +0200

    таблицы штрафов и разделки в админке + модели

---
+++ bc-cms/modules/Animals/Models/AnimalFine.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Animals\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AnimalFine extends Model
+{
+    protected $table = 'bc_animal_fines';
+
+    protected $fillable = [
+        'animal_id',
+        'type',
+        'price',
+    ];
+
+    public function animal()
+    {
+        return $this->belongsTo(Animal::class, 'animal_id');
+    }
+}
