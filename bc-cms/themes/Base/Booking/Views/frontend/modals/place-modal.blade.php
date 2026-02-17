7bf7c6e826b3e4ac401fb1257eb2129a0886ccb2
melvin-rulit <melvin.melvi.rulit@gmail.com>
2026-02-17 18:41:43 +0200

    модалка для кой-ко место

---
+++ bc-cms/themes/Base/Booking/Views/frontend/modals/place-modal.blade.php
@@ -0,0 +1,7 @@
+<div class="modal fade" id="placeBookingModal{{ $booking->id }}" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+        </div>
+    </div>
+</div>
