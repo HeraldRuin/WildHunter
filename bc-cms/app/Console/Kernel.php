c15461bc33e7005a19870ee373042c61b8c24285
melvin-rulit <melvin.melvi.rulit@gmail.com>
2026-02-23 10:10:17 +0200

    крон дл запуска проверки таймера койко-мест каждую минуту

--- bc-cms/app/Console/Kernel.php
+++ bc-cms/app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command(ScanUserPlanExpiredCommand::class)->daily()->withoutOverlapping();
-        $schedule->command('beds:process-expired')->everyMinute();
+        $schedule->command(ProcessExpiredBedsTimers::class)->everyMinute();
     }
 
     /**
