847f75f4092b9c2e14fbe0a20d1bd851ab92cc0d
melvin <melvin.melvin.rulit@gmail.com>
2026-03-18 11:14:54 +0000

    вынес калькуля/цию в отдельный сервис и метод перевода брони в статус завершения выбора коек, когда при работающем таймере сбора койки места выбираю места

--- bc-cms/modules/Booking/Controllers/BookingController.php
+++ bc-cms/modules/Booking/Controllers/BookingController.php
@@ -30,6 +30,7 @@ use Modules\Booking\Models\BookingHunterInvitation;
 use Modules\Booking\Models\BookingPassenger;
 use Modules\Booking\Models\BookingRoomPlace;
 use Modules\Booking\Models\BookingService;
+use Modules\Booking\Services\BookingCalculatingService;
 use Modules\Booking\Services\BookingTimerService;
 use Modules\Hotel\Models\HotelAnimal;
 use Modules\Hotel\Models\HotelRoomBooking;
@@ -50,13 +51,15 @@ class BookingController extends \App\Http\Controllers\Controller
     protected $bookingInst;
     protected $animalClass;
     protected $bookingTimerService;
+    protected $bookingCalculatingService;
 
-    public function __construct(Booking $booking, Enquiry $enquiryClass, Animal $animalClass, BookingTimerService $bookingTimerService)
+    public function __construct(Booking $booking, Enquiry $enquiryClass, Animal $animalClass, BookingTimerService $bookingTimerService, BookingCalculatingService $bookingCalculatingService)
     {
         $this->booking = $booking;
         $this->enquiryClass = $enquiryClass;
         $this->animalClass = $animalClass;
         $this->bookingTimerService = $bookingTimerService;
+        $this->bookingCalculatingService = $bookingCalculatingService;
     }
 
     protected function validateCheckout($code)
@@ -2177,6 +2180,7 @@ class BookingController extends \App\Http\Controllers\Controller
                 'user_id'      => auth()->id(),
             ]);
 
+            $this->updateStatusIfAllPlacesSelected($booking);
 
             $rooms = $booking
                 ->roomsBooking()
@@ -2222,6 +2226,17 @@ class BookingController extends \App\Http\Controllers\Controller
         }
     }
 
+    public function updateStatusIfAllPlacesSelected($booking): void
+    {
+        $paidCount = $booking->countAcceptedAndPaidHunters();
+        $alreadyHasPlace = BookingRoomPlace::where('booking_id', $booking->id)->count() === $paidCount;
+
+        if ($paidCount > 0 && $paidCount === $alreadyHasPlace) {
+            $booking->status = Booking::FINISHED_BED;
+            $booking->save();
+        }
+    }
+
     public function cancelSelectPlace(Request $request, $bookingId)
     {
         BookingRoomPlace::where('booking_id', $bookingId)->where('id', $request->input('place_id'))->where('user_id', auth()->id())->delete();
@@ -2230,235 +2245,8 @@ class BookingController extends \App\Http\Controllers\Controller
     //Калькуляция
     public function getCalculating(Booking $booking): JsonResponse
     {
-        $user = Auth::user();
-        $isBaseAdmin = $user->hasRole('baseadmin');
-        $masterBookingHunter = BookingHunter::where('booking_id', $booking->id)
-            ->where('is_master', true)
-            ->first();
+        $result = $this->bookingCalculatingService->calculate($booking, Auth::user());
 
-        $places = BookingRoomPlace::where('booking_id', $booking->id)->get();
-        $rooms = $places->groupBy('room_id');
-
-        $roomPrices = HotelRoomBooking::where('booking_id', $booking->id)
-            ->whereIn('room_id', $rooms->keys())
-            ->pluck('price', 'room_id');
-
-        $result = [];
-
-        foreach ($rooms as $roomId => $roomPlaces) {
-            $totalPlaces = $roomPlaces->count();
-            $myPlaces = $roomPlaces->where('user_id', $user->id)->count();
-
-            $roomPrice = $roomPrices[$roomId] ?? 0;
-            $roomPriceAllDay = $roomPrice * $booking->duration_days;
-
-            $pricePerPlace = $totalPlaces > 0 ? $roomPriceAllDay / $totalPlaces : 0;
-            $myCost = $pricePerPlace * $myPlaces;
-
-            $result[] = [
-                'room_id' => $roomId,
-                'total_places' => $totalPlaces,
-                'my_places' => $myPlaces,
-                'price_per_place' => round($pricePerPlace),
-                'my_cost' => round($myCost),
-            ];
-        }
-
-        $totalMyCost = array_sum(array_column($result, 'my_cost'));
-
-        BookingHunterInvitation::where('booking_hunter_id', $masterBookingHunter->id)
-            ->where('hunter_id', Auth::id())
-            ->update(['prepayment_paid' => true]);
-
-        $acceptedInvitations = $masterBookingHunter->invitations->where('status', 'accepted');
-
-        $paidCount = $acceptedInvitations->where('prepayment_paid', true)->count();
-        $services = BookingService::where('booking_id', $booking->id)->get();
-        $grouped = $services->groupBy('service_type');
-
-        $trophies = [];
-        $penalties = [];
-        $meals = [];
-        $preparations = [];
-        $spendings = [];
-        $addetionals = [];
-
-        // Трофеи
-        if ($grouped->has('trophy')) {
-
-            foreach ($grouped['trophy'] as $trophy) {
-                $animalName = optional(Animal::find($trophy->animal))->title ?? '';
-
-                $trophies[] = [
-                    'name' => $animalName . '  (' . $trophy->type. '  ' . 'x' . ' ' . $trophy->count . 'шт)',
-                    'total_cost' => round((float)$trophy->price ),
-                    'my_cost' => round($trophy->price / $paidCount),
-                ];
-            }
-        }
-
-        // Штрафы
-        if ($grouped->has('penalty')) {
-            $groupedByAnimalType = $grouped['penalty']->groupBy(function ($item) {
-                return $item->animal_id . '|' . $item->type;
-            });
-
-            foreach ($groupedByAnimalType as $key => $items) {
-                $first = $items->first();
-                $totalCost = $items->sum('price');
-                $currentHunterId = auth()->id();
-                $myCost = $items->where('hunter_id', $currentHunterId)->sum('price');
-
-                $penalties[] = [
-                    'name'       => $first->type,
-                    'total_cost' => round($totalCost),
-                    'my_cost'    => round($myCost),
-                ];
-            }
-        }
-
-        // Питание
-        if ($grouped->has('food')) {
-
-            foreach ($grouped['food'] as $foods) {
-                $totalCost = round((float)$foods->price * (int)$paidCount * (int)$booking->duration_days);
-                $meals[] = [
-                    'name' => $foods->type,
-                    'total_cost' => $totalCost,
-                    'my_cost' => round($totalCost / (int)$paidCount),
-                ];
-            }
-        }
-
-        // Разделка
-        if ($grouped->has('preparation')) {
-
-            foreach ($grouped['preparation'] as $preparation) {
-
-                $preparations[] = [
-                    'name' => 'Разделка',
-                    'total_cost' => round($preparation->price),
-                    'my_cost' => round($preparation->price / (int)$paidCount),
-                ];
-            }
-        }
-
-        // Доп услуги
-        if ($grouped->has('addetional')) {
-
-            foreach ($grouped['addetional'] as $addetional) {
-
-                $addetionals[] = [
-                    'name' => $addetional->type,
-                    'total_cost' => round($addetional->price),
-                    'my_cost' => round($addetional->price / max(1, (int)$paidCount)),
-                ];
-            }
-        }
-
-        // Трата охотников
-        $totalMyDebt = 0;
-        $totalSpending = 0;
-        if ($grouped->has('spending')) {
-
-            foreach ($grouped['spending'] as $spending) {
-                $hunter = User::find($spending->hunter_id);
-                $myCost = $spending->hunter_id === auth()->id()
-                    ? 0
-                    : round($spending->price / (int)$paidCount);
-
-                $totalMyDebt += $myCost;
-                $totalSpending += $spending->price;
-
-                $spendings[] = [
-                    'name' => ($hunter->last_name ?? '—') . ' (' . ($spending->comment ?? '') . ')',
-                    'total_cost' => round($spending->price),
-                    'my_cost'    => $myCost,
-                ];
-            }
-        }
-
-        $trophiesTotal      = array_sum(array_column($trophies, 'total_cost'));
-        $penaltiesTotal     = array_sum(array_column($penalties, 'total_cost'));
-        $addetionalsTotal   = array_sum(array_column($addetionals, 'total_cost'));
-        $preparationTotal   = array_sum(array_column($preparations, 'total_cost'));
-        $mealsTotal   = array_sum(array_column($meals, 'total_cost'));
-
-        $trophiesMyTotal      = array_sum(array_column($trophies, 'my_cost'));
-        $penaltiesMyTotal     = array_sum(array_column($penalties, 'my_cost'));
-        $addetionalsMyTotal   = array_sum(array_column($addetionals, 'my_cost'));
-        $preparationMyTotal   = array_sum(array_column($preparations, 'my_cost'));
-        $mealsMyTotal   = array_sum(array_column($meals, 'my_cost'));
-
-        $huntingAmountPaid = ($booking->amount_hunting / $booking->total_hunting) * (int)$paidCount;
-        $baseAmount = $booking->type === Booking::BookingTypeAnimal
-            ? round(
-                round($huntingAmountPaid)
-                + $trophiesTotal
-                + $penaltiesTotal
-                + $addetionalsTotal
-                + $preparationTotal
-                + $mealsTotal
-            )
-            : round(
-                $booking->total
-                + round($huntingAmountPaid)
-                + $trophiesTotal
-                + $penaltiesTotal
-                + $addetionalsTotal
-                + $preparationTotal
-                + $mealsTotal
-            );
-        $baseTotal = $baseAmount - $booking->total;
-        $huntingAmountMyPaid = round($huntingAmountPaid / (int)$paidCount);
-        $baseMyAmount = round($trophiesMyTotal + $penaltiesMyTotal + $addetionalsMyTotal + $preparationMyTotal + $mealsMyTotal);
-        $myPrepayment = round($booking->total / (int)$paidCount);
-        $baseMyTotalCost = round(($totalMyCost + $huntingAmountMyPaid + $baseMyAmount) - $myPrepayment);
-
-        $allItems = [
-            [
-                'name' => 'Внесена предоплата:',
-                'total_cost' => $booking->type === Booking::BookingTypeAnimal ? 0 : round($booking->total),
-                'my_cost' => $booking->type === Booking::BookingTypeAnimal ? 0 : $myPrepayment,
-            ],
-            [
-                'name' => 'Итог базе',
-                'total_cost' => $baseTotal,
-                'my_cost' => $baseMyTotalCost,
-            ],
-        ];
-
-        if (!$isBaseAdmin) {
-            $allItems[] = [
-                'name' => 'Итог охотникам',
-                'total_cost' => round($totalSpending),
-                'my_cost' => round($totalMyDebt),
-            ];
-        }
-
-        return response()->json([
-            'status' => true,
-            'is_baseAdmin' => $isBaseAdmin,
-            'items' => [
-                [
-                    'name' => $booking->type === Booking::BookingTypeAnimal ? 'Проживание, ' . plural_sutki(0) : 'Проживание, ' . plural_sutki($booking->duration_days),
-                    'total_cost' => $booking->type === Booking::BookingTypeAnimal ? 0 : round($booking->total),
-                    'my_cost' => $booking->type === Booking::BookingTypeAnimal ? 0 : $totalMyCost,
-                ],
-                [
-                    'name' => 'Организация охоты',
-                    'total_cost' => round($huntingAmountPaid),
-                    'my_cost' => $huntingAmountMyPaid,
-                    'has_tooltip' => true,
-                ],
-            ],
-            'trophies' => $trophies,
-            'penalties' => $penalties,
-            'meals' => $meals,
-            'preparation' => $preparations,
-            'addetionals' => $addetionals,
-            'spendings' => $spendings,
-            'all_items' => $allItems,
-        ]);
+        return response()->json($result);
     }
 }
