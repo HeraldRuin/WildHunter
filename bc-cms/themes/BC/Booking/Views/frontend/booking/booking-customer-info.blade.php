868930b1a62527a68c748c0cfc74c9333b2a3cf7
melvin <melvin.melvin.rulit@gmail.com>
2026-03-26 12:00:20 +0000

    особые требования вместо даных сделавший бронь

--- bc-cms/themes/BC/Booking/Views/frontend/booking/booking-customer-info.blade.php
+++ bc-cms/themes/BC/Booking/Views/frontend/booking/booking-customer-info.blade.php
@@ -2,34 +2,11 @@
     @if($ifAdminBase)
         <h4 class="booking-review-title">{{__('Client Information')}}</h4>
     @else
-        <h4 class="booking-review-title">{{__('Your Information')}}</h4>
+        <h4 class="booking-review-title">{{__('Special requirements')}}</h4>
     @endif
     <div class="booking-review-content">
-        <div class="review-section">
-            <div class="info-form">
-                <ul>
-                    <li class="info-first-name">
-                        <div class="label">{{__('First name')}}</div>
-                        <div class="val">{{$booking->first_name}}</div>
-                    </li>
-                    <li class="info-last-name">
-                        <div class="label">{{__('Last name')}}</div>
-                        <div class="val">{{$booking->last_name}}</div>
-                    </li>
-                    <li class="info-email">
-                        <div class="label">{{__('Email')}}</div>
-                        <div class="val">{{$booking->email}}</div>
-                    </li>
-                    <li class="info-phone">
-                        <div class="label">{{__('Phone')}}</div>
-                        <div class="val">{{$booking->phone}}</div>
-                    </li>
-                    <li class="info-address">
-                        <div class="label">{{__('Address')}}</div>
-                        <div class="val">{{$booking->address}}</div>
-                    </li>
-                </ul>
+            <div class="form-control" style="min-height:120px; background:#f8f9fa;">
+                {{ $booking->customer_notes ?? '—' }}
             </div>
-        </div>
     </div>
 </div>
