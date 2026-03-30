fd06bef8f933f5f4a0084a7d1fdb9534d814f9ce
melvin <melvin.melvin.rulit@gmail.com>
2026-03-30 12:45:01 +0000

    модуль оплаты

---
+++ bc-cms/modules/Booking/Gateways/PaykeeperGateway.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Modules\Booking\Gateways;
+
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Modules\Booking\Events\BookingCreatedEvent;
+use Modules\Booking\Models\Booking;
+use Modules\Booking\Models\Payment;
+use function Symfony\Component\Translation\t;
+
+class PaykeeperGateway extends BaseGateway
+{
+    public $name = 'Pay Keeper';
+
+    public function getOptionsConfigs()
+    {
+        return [
+            [
+                'type' => 'checkbox',
+                'id' => 'enable',
+                'label' => __('Enable PayKeeper?')
+            ],
+            [
+                'type'       => 'input',
+                'id'         => 'name',
+                'label'      => __('Custom Name'),
+                'std'        => __("PayKeeper"),
+                'multi_lang' => "1"
+            ],
+            [
+                'type'  => 'upload',
+                'id'    => 'logo_id',
+                'label' => __('Custom Logo'),
+            ],
+            [
+                'type'  => 'editor',
+                'id'    => 'html',
+                'label' => __('Custom HTML Description'),
+                'multi_lang' => "1"
+            ],
+            [
+                'type'  => 'checkbox',
+                'id'    => 'test',
+                'label' => __('Enable Sandbox Mode?')
+            ],
+            [
+                'type'    => 'select',
+                'id'      => 'convert_to',
+                'label'   => __('Convert To'),
+                'desc'    => __('In case of main currency does not support by PayPal. You must select currency and input exchange_rate to currency that PayPal support'),
+                'options' => $this->supportedCurrency()
+            ],
+            [
+                'type'       => 'input',
+                'input_type' => 'number',
+                'id'         => 'exchange_rate',
+                'label'      => __('Exchange Rate'),
+                'desc'       => __('Example: Main currency is VND (which does not support by PayPal), you may want to convert it to USD when customer checkout, so the exchange rate must be 23400 (1 USD ~ 23400 VND)'),
+            ],
+            [
+                'type'      => 'input',
+                'id'        => 'test_client_id',
+                'label'     => __('Sandbox Client Id'),
+                'condition' => 'g_paypal_test:is(1)'
+            ],
+            [
+                'type'      => 'input',
+                'id'        => 'test_client_secret',
+                'label'     => __('Sandbox Client Secret'),
+                'std'       => '',
+                'condition' => 'g_paypal_test:is(1)'
+            ],
+            [
+                'type'      => 'input',
+                'id'        => 'client_id',
+                'label'     => __('Client Id'),
+                'condition' => 'g_paypal_test:is()'
+            ],
+            [
+                'type'      => 'input',
+                'id'        => 'client_secret',
+                'label'     => __('Client Secret'),
+                'std'       => '',
+                'condition' => 'g_paypal_test:is()'
+            ],
+        ];
+    }
+
+
+
+}
