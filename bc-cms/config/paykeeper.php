4e003bc9fcc6586fd58a7ea82bf1d47c682bbb86
melvin <melvin.melvin.rulit@gmail.com>
2026-04-01 10:17:32 +0000

    настройки paykeeper

---
+++ bc-cms/config/paykeeper.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'base_url' => env('PAYKEEPER_BASE_URL'),
+    'success_url' => env('PAYKEEPER_SUCCESS_URL'),
+    'fail_url' => env('PAYKEEPER_FAIL_URL'),
+
+    'pay_ttl_days' => env('PAYKEEPER_PAY_TTL_DAYS', 1),
+    'token_ttl' => env('PAYKEEPER_TOKEN_TTL', 1440),
+    'cache_key' => env('PAYKEEPER_CACHE_KEY', 'paykeeper_token'),
+];
