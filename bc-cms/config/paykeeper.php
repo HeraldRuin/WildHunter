9dd2ff89d4051e288e2f75097d4a3396da453b1a
melvin <melvin.melvin.rulit@gmail.com>
2026-04-02 14:44:48 +0000

    ручка управления отправки чека пользователю на почту

--- bc-cms/config/paykeeper.php
+++ bc-cms/config/paykeeper.php
@@ -5,6 +5,8 @@ return [
     'success_url' => env('PAYKEEPER_SUCCESS_URL'),
     'fail_url' => env('PAYKEEPER_FAIL_URL'),
 
+    'send_check_email' => env('SEND_CHECK_EMAIL', false),
+
     'pay_ttl_days' => env('PAYKEEPER_PAY_TTL_DAYS', 1),
     'token_ttl' => env('PAYKEEPER_TOKEN_TTL', 1440),
     'cache_key' => env('PAYKEEPER_CACHE_KEY', 'paykeeper_token'),
