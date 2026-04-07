20d8092e85a0d244a03e471bd083f94710ac8539
melvin <melvin.melvin.rulit@gmail.com>
2026-04-07 10:42:14 +0000

    выношу логику в сервис

--- bc-cms/modules/Booking/Services/BookingCollectionService.php
+++ bc-cms/modules/Booking/Services/BookingCollectionService.php
@@ -3,8 +3,50 @@
 namespace Modules\Booking\Services;
 
 use Modules\Booking\Models\Booking;
+use Modules\Booking\Models\BookingHunterInvitation;
+use Modules\Booking\Models\BookingRoomPlace;
 
 class BookingCollectionService
 {
+    protected $bookingTimerService;
 
+    public function __construct(BookingTimerService $bookingTimerService)
+    {
+        $this->bookingTimerService = $bookingTimerService;
+    }
+    public function checkPrepaymentAllPaid(Booking $booking, ?BookingHunterInvitation $invitation = null): void
+    {
+        $unpaidHunters = $booking->unpaidInvitationsOfHunters();
+
+        if ($unpaidHunters->isEmpty()) {
+            $this->bookingTimerService->startBedTimer($booking);
+        }else {
+            $invitation->prepayment_paid_status = BookingHunterInvitation::PREPAYMENT_PENDING;
+            $invitation->save();
+            $this->bookingTimerService->startPaidTimer($booking);
+        }
+    }
+
+    public function updateStatusIfAllPlacesSelected(Booking $booking): void
+    {
+        $paidCount = $booking->countAcceptedAndPaidHunters();
+        $alreadyHasPlace = BookingRoomPlace::where('booking_id', $booking->id)->count() === $paidCount;
+
+        if ($paidCount > 0 && $paidCount === $alreadyHasPlace) {
+            $booking->status = Booking::FINISHED_BED;
+            $booking->save();
+        }
+    }
+
+    public function markAllPendingAsUnpaid(Booking $booking): void
+    {
+        $unpaidHunters = $booking->pendingInvitationsOfHunters();
+
+        if ($unpaidHunters->isNotEmpty()) {
+            $unpaidHunters->each(function (BookingHunterInvitation $invitation) {
+                $invitation->prepayment_paid_status = BookingHunterInvitation::PREPAYMENT_UNPAID;
+                $invitation->save();
+            });
+        }
+    }
 }
