62088da3b6b0096bdde559f9935978dfeb0cf085
melvin <melvin.melvin.rulit@gmail.com>
2026-04-08 18:42:40 +0000

    DTO для сервиса услуг

---
+++ bc-cms/modules/Booking/DTO/StorePreparationData.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\Booking\DTO;
+
+use Illuminate\Http\Request;
+
+class StorePreparationData
+{
+    public function __construct(
+        public int $preparation_id,
+        public int $animal_id,
+        public string $type,
+        public int $count,
+    ) {}
+
+    public static function fromRequest(Request $request): self
+    {
+        $data = $request->validated();
+
+        return new self(
+            preparation_id: (int) $data['preparation_id'],
+            animal_id: (int) $data['animal_id'],
+            type: $data['type'],
+            count: (int) $data['count'],
+        );
+    }
+}
