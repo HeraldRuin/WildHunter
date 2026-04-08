826f125552b0e507a2dec406044978315783ae00
melvin <melvin.melvin.rulit@gmail.com>
2026-04-08 16:32:29 +0000

    выношу валидацию в слой

---
+++ bc-cms/modules/Booking/Requests/StoreAddetionalRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Booking\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAddetionalRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'addetional'    => 'required|string',
+            'addetional_id' => 'required|integer|exists:bc_addetional_prices,id',
+        ];
+    }
+}
