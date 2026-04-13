dc46c14f7b6427ab33cb14c48586bf17335be0ef
melvin <melvin.melvin.rulit@gmail.com>
2026-04-13 14:37:28 +0000

    добавил cron для проверки статуса оплаты

--- bc-cms/routes/console.php
+++ bc-cms/routes/console.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Console\Scheduling\Schedule;
+use Modules\Booking\Jobs\CheckPaymentStatusJob;
 
 // Заполняет автоматически койки теми, кто не выбрал для себя место в номере
 app()->booted(function () {
     $schedule = app(Schedule::class);
     $schedule->command('beds:process-expired')->everyMinute()->timezone('Europe/Moscow');
+    $schedule->job(new CheckPaymentStatusJob())->everyMinute()->timezone('Europe/Moscow');
 });
