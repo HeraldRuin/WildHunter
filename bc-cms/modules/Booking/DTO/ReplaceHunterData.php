000a8c2899cc31d28503e9df5ba48b78acd8d1b4
melvin <melvin.melvin.rulit@gmail.com>
2026-04-24 07:54:45 +0000

    правка на нужные поля

--- bc-cms/modules/Booking/DTO/ReplaceHunterData.php
+++ bc-cms/modules/Booking/DTO/ReplaceHunterData.php
@@ -10,9 +10,8 @@ class ReplaceHunterData
     public function __construct(
         public int $newHunterId,
         public ?string $email,
-        public ?string $firstName,
-        public ?string $lastName,
         public ?string $userName,
+        public ?string $userNik,
         public ?string $invitationStatus,
         public bool $isExternal,
         public int $oldHunterId
@@ -25,9 +24,8 @@ class ReplaceHunterData
         return new self(
             newHunterId: (int) $hunter['id'],
             email: $hunter['email'] ?? null,
-            firstName: $hunter['first_name'] ?? null,
-            lastName: $hunter['last_name'] ?? null,
-            userName: $hunter['user_name'] ?? null,
+            userName: $hunter['name'] ?? null,
+            userNik: $hunter['user_name'] ?? null,
             invitationStatus: $hunter['invitation_status'] ?? null,
             isExternal: (bool) ($hunter['is_external'] ?? false),
             oldHunterId: (int) $request->input('old_hunter_id')
