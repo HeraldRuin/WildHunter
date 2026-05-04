96d2c6ba1800de3ab4294e8127384f6c6cd5497a
melvin <melvin.melvin.rulit@gmail.com>
2026-05-04 09:28:24 +0000

    DDD придерживание

--- bc-cms/modules/Booking/Services/BookingUserService.php
+++ bc-cms/modules/Booking/Services/BookingUserService.php
@@ -4,77 +4,62 @@ namespace Modules\Booking\Services;
 
 use App\Exceptions\ConflictException;
 use App\Exceptions\NotFoundException;
-use App\User;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
+use Modules\Booking\Application\UseCases\Hunters\ReplaceHunterResult;
 use Modules\Booking\DTO\ReplaceHunterData;
 use Modules\Booking\Models\Booking;
 use Modules\Booking\Models\BookingHunterInvitation;
+use Modules\Booking\Repositories\BookingHunterRepository;
+use Modules\Booking\Repositories\BookingRepository;
+use Modules\User\Repositories\UserRepository;
 
 class BookingUserService
 {
-    public function __construct(protected BookingCollectionService $bookingCollectionService){}
+    public function __construct(
+        protected BookingCollectionService $bookingCollectionService,
+        private UserRepository $userRepository,
+        private BookingRepository $bookingRepository,
+        private BookingHunterRepository $bookingHunterRepository) {}
 
-    /**
-     * @throws NotFoundException
-     */
     public function changeUser(Booking $booking, int $userId): array
     {
-        $booking->create_user = $userId;
-        $booking->save();
+        return DB::transaction(function () use ($booking, $userId) {
 
-        $user = User::find($userId);
+            $user = $this->userRepository->findHunterById($userId);
 
-        if (!$user) {
-            throw new NotFoundException(
-                errorCode: 'user_not_found',
-                domain: 'booking'
-            );
-        }
+            if (!$user) {
+                throw new NotFoundException(
+                    errorCode: 'user_not_found',
+                    domain: 'booking'
+                );
+            }
 
-        $bookingHunter = $booking->masterHunter;
+            $booking->changeCreator($user);
+            $booking->changeMasterHunterCreator($user);
 
-        if (!$bookingHunter) {
-            throw new NotFoundException(
-                errorCode: 'master_not_found',
-                domain: 'booking'
-            );
-        }
+            $this->bookingRepository->save($booking);
+            $this->bookingHunterRepository->save($booking->masterHunter);
 
-        $bookingHunter->invited_by = $userId;
-        $bookingHunter->is_master = $user->hasRole('hunter');
-        $bookingHunter->creator_role = $user->role->code ?? null;
-        $bookingHunter->save();
+            return [
+                'code' => 'customer_changed',
+            ];
+        });
 
-        return [
-            'code' => 'customer_changed',
-        ];
+
     }
 
     public function searchHunters(string $query, int $bookingId = null): Collection
     {
-        $users = User::query()
-            ->whereNotIn('role_id', function ($q) {
-                $q->select('id')
-                    ->from('core_roles')
-                    ->whereIn('code', ['administrator']);
-            })
-            ->where(function ($q) use ($query) {
-                $q->where('user_name', 'LIKE', $query.'%')
-                    ->orWhere('first_name', 'LIKE', $query.'%')
-                    ->orWhere('last_name', 'LIKE', $query.'%')
-                    ->orWhere('email', 'LIKE', $query.'%')
-                    ->orWhere('id', 'LIKE', $query.'%');
-            })
-            ->limit(10)
-            ->get(['id','user_name','first_name','last_name','email','phone']);
+        $users = $this->userRepository->searchHuntersByQuery($query);
 
-        $users->each(function ($user) {
-            $user->invited = false;
-            $user->invitation_status = null;
-        });
+//        $users->each(function ($user) {
+//            $user->invited = false;
+//            $user->invitation_status = null;
+//        });
 
         if ($bookingId) {
-            $this->applyInvitationStatus($users, $bookingId);
+//            $this->applyInvitationStatus($users, $bookingId);
         }
 
         return $users;
@@ -105,51 +90,28 @@ class BookingUserService
         }
     }
 
+
     /**
-     * @throws \Exception
+     * @throws ConflictException
+     * @throws NotFoundException
      */
-    public function replaceHunter(Booking $booking, ReplaceHunterData $data): array
+    public function replaceHunter(Booking $booking, ReplaceHunterData $data): ReplaceHunterResult
     {
-        if ($booking->invitationUser($data->newHunterId)) {
-            throw new ConflictException(
-                errorCode: 'hunter_already_in_booking',
-                domain: 'booking'
-            );
+        $invitation = $booking->replaceHunter(
+            oldHunterId: $data->oldHunterId,
+            newHunterId: $data->newHunterId,
+            email: $data->email
+        );
+
+        $invitation->save();
+
+        if ($booking->shouldCheckPrepayment()){
+             $this->bookingCollectionService->checkPrepaymentAllPaid($booking, $invitation);
         }
 
-        $invitation = $booking->invitationUser($data->oldHunterId);
-
-        if (!$invitation) {
-            throw new NotFoundException(
-                errorCode: 'booking_invitation_not_found',
-                domain: 'booking'
-            );
-        }
-
-            $invitation->hunter_id = $data->newHunterId;
-            $invitation->email = !empty($data->email) ? $data->email : null;
-            $invitation->save();
-
-            if ($booking->shouldCheckPrepayment()){
-                $this->bookingCollectionService->checkPrepaymentAllPaid($booking, $invitation);
-            }
-
-        return [
-            'code' => 'hunter_replace',
-            'data' => [
-                'hunter' => [
-                    'id' => $data->newHunterId,
-                    'email' => $invitation->email?? null,
-                    'first_name' => $data->firstName?? null,
-                    'last_name' => $data->lastName?? null,
-                    'user_name' => $data->userNik?? null,
-                    'is_external' => $data->isExternal ?? false,
-                    'invitation_status' => $data->invitationStatus ?? BookingHunterInvitation::STATUS_ACCEPTED,
-                    'prepayment_paid' => (bool) ($invitation->prepayment_paid ?? false),
-                    'prepayment_paid_status' => $invitation->prepayment_paid_status,
-                    'prepayment_badge' => $invitation->prepayment_badge,
-                ],
-            ],
-        ];
+        return new ReplaceHunterResult(
+            invitation: $invitation,
+            dto: $data
+        );
     }
 }
